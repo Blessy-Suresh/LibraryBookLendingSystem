035d9d42fc1ba3e1690f8579b792ad5b392d55be
Blessy Mol Suresh <blessymol268@gmail.com>
2026-02-02 11:59:08 +0530

    Delete WarehouseSystem directory

--- WarehouseSystem/src/com/kce/warehouse/exception/WarehouseValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.kce.warehouse.exception;
-
-public class WarehouseValidationException extends Exception {
-    @Override
-    public String toString() {
-        return "Invalid input data!";
-    }
-}
