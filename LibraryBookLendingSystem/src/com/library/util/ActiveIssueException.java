cb2bfb3debc39255773fa532f3e7b58507826bae
bless <bless@HP>
2026-02-02 11:54:53 +0530

    initial commit

---
+++ LibraryBookLendingSystem/src/com/library/util/ActiveIssueException.java
@@ -0,0 +1,8 @@
+package com.library.util;
+
+public class ActiveIssueException {
+	@Override
+	public String toString() {
+	return "Book has active issues and cannot be deleted";
+}
+}
---
+++ LibraryBookLendingSystem/src/com/library/util/BookUnavailableException.java
@@ -0,0 +1,8 @@
+package com.library.util;
+
+public class BookUnavailableException {
+	@Override
+	public String toString() {
+		return "Book not available for issue";
+	}
+}
